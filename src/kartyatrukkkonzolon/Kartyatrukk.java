00e66bf6e14bef80d6fa71610e6738679936cc46
Christuss <szendefi.krisztofer@gmail.com>
2023-09-18 15:45:55 +0200

    pakli, lap metoudsainak feltoltese

--- src/kartyatrukkkonzolon/Kartyatrukk.java
+++ src/kartyatrukkkonzolon/Kartyatrukk.java
@@ -13,6 +13,13 @@ public class Kartyatrukk {
         
     }
     public void melyik() {
-        
+        boolean jo;
+        int oszlop;
+        do {
+            System.out.print("melyik oszlop (1-3): ");
+            oszlop = sc.nextInt();
+            jo = oszlop >= 1 && oszlop <= 3;
+        } while (!jo);
+        return oszlop;
     }
 }
