6ccf27039596d62b711af9dddc18c56be0c79b17
Christuss <szendefi.krisztofer@gmail.com>
2023-09-18 15:21:08 +0200

    pakli, lap, kartyatrukk osztály alap

--- src/kartyatrukkkonzolon/Kartyatrukk.java
+++ src/kartyatrukkkonzolon/Kartyatrukk.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package kartyatrukkkonzolon;
 
-/**
- *
- * @author szendefi.k.daniel
- */
+import java.util.Scanner;
+
 public class Kartyatrukk {
+    private Scanner sc = new Scanner(System.in);
+
+    public Kartyatrukk() {
+    }
     
+    public void indit() {
+        
+    }
+    public void melyik() {
+        
+    }
 }
--- src/kartyatrukkkonzolon/Pakli.java
+++ src/kartyatrukkkonzolon/Pakli.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package kartyatrukkkonzolon;
 
-/**
- *
- * @author szendefi.k.daniel
- */
+
 public class Pakli {
+
+    public Pakli() {
+    }
     
+    public void feltolt() {
+        
+    }
+    public void kirak() {
+        
+    }
+    public void kever() {
+        
+    }
+    public void ezVolt() {
+        
+    }
 }
